721cc2ab1913e4d7039bf8f3b96a062c19880e21
Leo <62186360+AlexCole999@users.noreply.github.com>
2024-03-22 01:20:04 +0500

    test

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'u4757307_user',
-  password: 'f7{ci{_oO%f;',
-  database: 'u4757307_database1'
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err);
-    return;
-  }
-  console.log('Connected to MySQL');
-});
+setInterval(() => console.log('tick'), 250)
