7be94b3c99b35940b9a8de2fae31c7aff109902e
Leo <62186360+AlexCole999@users.noreply.github.com>
2024-03-21 22:42:45 +0500

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: 'sh4757307.c.had.su',
+  host: 'localhost',
   user: 'u4757307_user',
   password: 'f7{ci{_oO%f;',
   database: 'u4757307_database1'
